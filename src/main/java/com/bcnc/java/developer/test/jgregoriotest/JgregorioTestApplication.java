53dd410081c88cc170203ff23e79aca458b175a8
Jesus Gregorio <gregoriojesus0@gmail.com>
2024-02-12 15:56:38 +0100

    Add Spring Boot Application

---
+++ src/main/java/com/bcnc/java/developer/test/jgregoriotest/JgregorioTestApplication.java
@@ -0,0 +1,13 @@
+package com.bcnc.java.developer.test.jgregoriotest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JgregorioTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JgregorioTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bcnc/java/developer/test/jgregoriotest/JgregorioTestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bcnc.java.developer.test.jgregoriotest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+class JgregorioTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+        assertTrue(true);
+    }
+
+}
